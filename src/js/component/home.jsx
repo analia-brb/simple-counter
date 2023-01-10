f41765ec581b9b03dba1f8c2644a42f38464c940
analia-brb <analia.brb@gmail.com>
2023-01-10 13:37:33 +0000

    Simple Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 
+
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-dark" style={{height:"150px"}}>
+			<div className="row text-light text-center"><h1>Simple Counter</h1></div>
+			<div className="row d-flex justify-content-center">
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "60px", fontSize: "30px"}}>🕥</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop6}</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop5}</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop4}</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop3}</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop2}</div>
+				<div className="bg-secondary d-flex justify-content-center align-items-center m-2" style={{color: "white", height: "50px", width: "50px", fontSize: "30px"}}>{props.prop1}</div>
+							</div>
+			
+
 		</div>
+		
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,62 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let numero1 = 0;
+let numero2 = 0;
+let numero3 = 0;
+let numero4 = 0;
+let numero5 = 0;
+let numero6 = 0;
+
+setInterval(function() {
+            numero1++;
+            if (numero1 > 9) {
+                numero1 = 0
+                numero2++
+                if (numero2 > 9) {
+                    numero2 = 0
+                    numero3++
+                    if (numero3 > 9) {
+                        numero3 = 0
+                        numero4++
+                        if (numero4 > 9) {
+                            numero4 = 0
+                            numero5++
+                            if (numero5 > 9) {
+                                numero5 = 0
+                                numero6++
+                                if (numero6 > 9) {
+                                    numero6 = 0
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+
+            ReactDOM.render( <
+                Home prop1 = {
+                    numero1
+                }
+                prop2 = {
+                    numero2
+                }
+                prop3 = {
+                    numero3
+                }
+                prop4 = {
+                    numero4
+                }
+                prop5 = {
+                    numero5
+                }
+                prop6 = {
+                    numero6
+                }
+                / > , document.querySelector("#app"));
+            }, 1000)
